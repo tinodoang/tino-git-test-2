14e974502e150f3c74b55138352da5bf57ef241d
Maretino <maretino@gmail.com>
2024-04-03 14:19:52 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>coba git remote 2</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis recusandae totam dignissimos fuga! Et repellendus ex eius iusto nam ducimus dolore! Voluptatem laboriosam autem odit. Voluptas minima odit sed quaerat nemo, error autem, at in rerum eos reiciendis ab sunt a iusto incidunt. Quo doloribus inventore consectetur cumque rem est officiis perferendis unde ea aliquam sapiente sed consequatur qui porro veritatis, adipisci a, veniam nisi! Tempore rerum labore ut harum ea illo. Ducimus assumenda cum consectetur! Fugiat autem porro maxime in ratione repudiandae blanditiis laboriosam vel, magnam deleniti quisquam amet animi velit officia. Rem architecto doloremque neque. Obcaecati, expedita aperiam.</p>
 </body>
 </html>
